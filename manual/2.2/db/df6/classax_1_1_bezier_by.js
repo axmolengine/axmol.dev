b571fe606fb5e9764ae1334c354eff03ff3d5db1
halx99 <halx99@users.noreply.github.com>
2025-05-10 02:25:29 +0000

    deploy: cccbe0113f207174a3245d6544eb1904de2d496d

---
+++ manual/2.2/db/df6/classax_1_1_bezier_by.js
@@ -0,0 +1,5 @@
+var classax_1_1_bezier_by =
+[
+    [ "update", "db/df6/classax_1_1_bezier_by.html#a751ebb37cca9cecea95c8160529b097c", null ],
+    [ "initWithDuration", "db/df6/classax_1_1_bezier_by.html#a9b5de086d4e441ce0e265b1c934000e4", null ]
+];
