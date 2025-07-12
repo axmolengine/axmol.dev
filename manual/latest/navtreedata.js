0eb046e1a6bf1275b255373c4f68325b4f7eb992
halx99 <halx99@users.noreply.github.com>
2025-07-12 15:21:14 +0000

    deploy: 707e9d5cfe90b5ce04e573d9cc1f0c506c30d7d8

---
+++ manual/latest/navtreedata.js
@@ -0,0 +1,78 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Axmol Engine", "index.html", [
+    [ "axmol", "index.html", "index" ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Namespace Members", "namespacemembers.html", [
+      [ "All", "namespacemembers.html", null ],
+      [ "Functions", "namespacemembers_func.html", null ],
+      [ "Variables", "namespacemembers_vars.html", null ],
+      [ "Typedefs", "namespacemembers_type.html", null ],
+      [ "Enumerations", "namespacemembers_enum.html", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d1/d61/classax_1_1_mesh_skin.html",
+"d2/d1f/classax_1_1_label.html#aadf73dfdd41e1754ef0e2c65dff30755",
+"d3/d02/classax_1_1_label_atlas.html#aeddd54e6c5bbad30ccad004f2800a86a",
+"d3/d6f/classax_1_1_vec2.html#ac06a3e0dd0c6e3fdead39b4efa9e2545",
+"d4/d59/classax_1_1ui_1_1_edit_box.html#a9863ef0820e2cdf6f8d976755a67821a",
+"d4/ddd/classax_1_1ui_1_1_rich_text.html#a7d28c4dcd8e7463d6c057bc3c5e8ce26",
+"d6/d27/classax_1_1_transition_fade_t_r.html#acc715794c147e336a18121e8fb676c4d",
+"d7/d27/classax_1_1_event_keyboard.html#a7885f47644a0388f981f416fa20389b2",
+"d8/d39/classax_1_1_particle_system.html#a1c89b6bc43701762bb9de4104e08ab41",
+"d8/d6e/classax_1_1_mat4.html#abf5294f4441531747d3fd7c003f52d09",
+"d9/d4a/classax_1_1_camera_background_brush.html#a4736dae5ba8dd2408be19b4d886cfb44",
+"da/d34/classax_1_1_sprite.html#a52b7741f1ccd38d665e153882b7dc0dd",
+"da/df0/classax_1_1_event_listener.html#a7146e3a987f964ef9fa7b8009ed10fe3",
+"dc/d08/classax_1_1ui_1_1_loading_bar.html#aca8e116555cda9840b196bebec398cd7",
+"dc/dcb/classax_1_1_ease_sine_in_out.html",
+"dd/dd4/classax_1_1_particle_batch_node.html#a9bc3fb695690f6b9a722415004eefb24",
+"de/dc1/classax_1_1ui_1_1_rich_element_text.html#a34392c71ac2fffc011a24c7536a3e039",
+"df/da2/classax_1_1_node.html#a36c2aac96becc8765d136768c99887b2",
+"functions_vars.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
