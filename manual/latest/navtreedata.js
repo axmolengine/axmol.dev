1053b9fe03eecd905ac90167efc262c2dc000c07
halx99 <halx99@users.noreply.github.com>
2025-08-09 02:58:40 +0000

    deploy: 9721855d4712c11d210302f1946abace77c06b4c

---
+++ manual/latest/navtreedata.js
@@ -0,0 +1,76 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Axmol Engine", "index.html", [
+    [ "axmol", "index.html", "index" ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Namespace Members", "namespacemembers.html", [
+      [ "All", "namespacemembers.html", null ],
+      [ "Functions", "namespacemembers_func.html", null ],
+      [ "Variables", "namespacemembers_vars.html", null ],
+      [ "Enumerations", "namespacemembers_enum.html", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d1/d61/classax_1_1_mesh_skin.html#a5facee5ee12a625f2af022da99de1e63",
+"d2/d1f/classax_1_1_label.html#abc8341636c73d68315750bb0db1f5ace",
+"d3/d0c/classax_1_1ui_1_1_scroll_view.html#a224b9163917ac32fc95a60d8c1eec3aa",
+"d3/d76/classax_1_1_atlas_node.html#a14b5de2ade7ecad8a91e7a84d42e541b",
+"d4/d59/classax_1_1ui_1_1_edit_box.html#ad5e07247f22aba7086afd91a16c599b0",
+"d4/ddd/classax_1_1ui_1_1_rich_text.html#af158fdf0e84b409ded5daff6d0f89fbc",
+"d5/dfa/classax_1_1_cardinal_spline_to.html#a751ebb37cca9cecea95c8160529b097c",
+"d7/d0a/classax_1_1network_1_1_uri.html#a6e5cba912a8b0dc0205c4287c05ad5da",
+"d8/d39/classax_1_1_particle_system.html#a0b6c729af6d0edd3c39e670a8b7636cb",
+"d8/d6e/classax_1_1_mat4.html#a89e2e181668f2487d80b4ffcb4abaa47",
+"d9/d3b/classax_1_1_motion_streak3_d.html#acc9adf2a1100ea7fdc084df4ae902d23",
+"da/d34/classax_1_1_sprite.html#a3eaacdcdb32cf2de74fbedaaad63f8e0",
+"da/df0/classax_1_1_event_listener.html#a1d1cfd8ffb84e947f82999c682b666a7",
+"dc/d08/classax_1_1ui_1_1_loading_bar.html#a81ade790dd930d0c000bf24bddf04470",
+"dc/dd7/structax_1_1_v2_f___c4_b___t2_f___triangle.html",
+"dd/df8/classax_1_1ui_1_1_list_view.html#a6a6dd22bf18a9df39dc76d428809dbe1",
+"de/dd7/classax_1_1_audio_decoder_wav.html#afed100fa54a9a67fe17ff7f172300849",
+"df/da2/classax_1_1_node.html#a818b0254d354593ea2319a2c28b32f2e"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
