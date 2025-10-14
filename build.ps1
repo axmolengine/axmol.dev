62491264cdbbd463b746d00f0ed2dc09878f6b32
halx99 <halx99@live.com>
2025-10-14 17:10:59 +0800

    Deploy build pages to repo axmol.dev.dist

--- build.ps1
+++ build.ps1
@@ -1,55 +1,52 @@
 param(
-    $axmol_src = $null,
-    $wasm_artifact_dir = $null,
-    $min_doc_ver = '2.4'
+  $axmol_src = $null,
+  $wasm_artifact_dir = $null,
+  $min_doc_ver = '2.5'
 )
 if ($wasm_artifact_dir) {
-    $wasm_artifact_dir = (Resolve-Path $wasm_artifact_dir).Path
+  $wasm_artifact_dir = (Resolve-Path $wasm_artifact_dir).Path
 }
-
-$my_root = $PSScriptRoot
 
 function mkdirs([string]$path) {
-    if (!(Test-Path $path)) {
-        New-Item $path -ItemType Directory 1>$null
-    }
+  if (!(Test-Path $path)) {
+    New-Item $path -ItemType Directory 1>$null
+  }
 }
 
-$site1_dist = Join-Path $my_root 'dist/site1'
-mkdirs $site1_dist
+$site_dist = Join-Path $PSScriptRoot 'dist/'
+mkdirs $site_dist
 
 # step.1 build main site
-Copy-Item (Join-Path $my_root 'index.html') $site1_dist
-Copy-Item (Join-Path $my_root 'assets') $site1_dist -Recurse -Force
-Copy-Item (Join-Path $my_root 'donate') $site1_dist -Recurse -Force
+Copy-Item (Join-Path $PSScriptRoot 'index.html') $site_dist
+Copy-Item (Join-Path $PSScriptRoot 'assets') $site_dist -Recurse -Force
+Copy-Item (Join-Path $PSScriptRoot 'donate') $site_dist -Recurse -Force
 
 # step.2 build docs to main site manual
 if ($axmol_src) {
-    $build_docs_script = Join-Path $axmol_src 'tools/ci/build-docs.ps1'
-    &$build_docs_script $site1_dist -min_ver $min_doc_ver
+  $build_docs_script = Join-Path $axmol_src 'tools/ci/build-docs.ps1'
+  &$build_docs_script $site_dist -min_ver $min_doc_ver
 }
 
 # step.3 build wasm preview site
 
-if($wasm_artifact_dir) {
-    $site2_dist = Join-Path $my_root 'dist/site2'
+if ($wasm_artifact_dir) {
+  Copy-Item $(Join-Path $PSScriptRoot 'wasm') $site_dist -Recurse -Force
 
-    # build site_dist2 aka isolated site wasm demos preview with pthread support
-        $site2_wasm_dir = Join-Path $site2_dist 'wasm/'
-        mkdirs $site2_wasm_dir
-        Copy-Item $(Join-Path $my_root 'wasm/index.html') $(Join-Path $site2_dist 'index.html')
-        Copy-Item $(Join-Path $my_root 'wasm/_headers') $site2_dist
-        function copy_tree_if($source, $dest) {
-            if (Test-Path $source) {
-                Copy-Item $source $dest -Container -Recurse
-            }
-        }
+  # add headers config for wasm pthread support, current netlify support
+  # github pages not support configure custom headers
+  Copy-Item $(Join-Path $PSScriptRoot '_headers') $site_dist
+  $site_wasm_dir = Join-Path $site_dist 'wasm/'
+  function copy_tree_if($source, $dest) {
+    if (Test-Path $source) {
+      Copy-Item $source $dest -Container -Recurse
+    }
+  }
 
-        $cpp_tests_dir = $(Join-Path $wasm_artifact_dir 'cpp-tests')
-        if (!(Test-Path $cpp_tests_dir -PathType Container)) {
-            throw "Missing wasm cpp-tests, caused by last wasm ci build fail."
-        }
-        copy_tree_if $cpp_tests_dir $site2_wasm_dir
-        copy_tree_if $(Join-Path $wasm_artifact_dir 'fairygui-tests') $site2_wasm_dir
-        copy_tree_if $(Join-Path $wasm_artifact_dir 'HelloLua') $site2_wasm_dir
+  $cpp_tests_dir = $(Join-Path $wasm_artifact_dir 'cpp-tests')
+  if (!(Test-Path $cpp_tests_dir -PathType Container)) {
+    throw "Missing wasm cpp-tests, caused by last wasm ci build fail."
+  }
+  copy_tree_if $cpp_tests_dir $site_wasm_dir
+  copy_tree_if $(Join-Path $wasm_artifact_dir 'fairygui-tests') $site_wasm_dir
+  copy_tree_if $(Join-Path $wasm_artifact_dir 'HelloLua') $site_wasm_dir
 }
